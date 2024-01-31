d033cca709199ccdaa2dcc7dba1e0d68b0bba518
wenovus <wenbli@google.com>
2024-01-30 21:51:04 -0800

    Improve debug stmt

--- internal/pkg/table/policy.go
+++ internal/pkg/table/policy.go
@@ -2667,7 +2667,8 @@ func (s *Statement) Apply(logger log.Logger, path *Path, options *PolicyOptions)
 	defer func() {
 		var condDesc strings.Builder
 		for _, c := range s.Conditions {
-			condDesc.WriteString(fmt.Sprintf("(%T, %s): %s\n", c, c.Name(), c.Set().String()))
+			condResult := !c.Evaluate(path, options)
+			condDesc.WriteString(fmt.Sprintf("(%T, %s): %s (condition matched: %v)\n", c, c.Name(), c.Set().String(), condResult))
 		}
 		routeAction := "<nil>"
 		if !reflect.ValueOf(s.RouteAction).IsNil() {
